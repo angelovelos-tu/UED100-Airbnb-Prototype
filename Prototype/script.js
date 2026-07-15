a603c00e6bcd4b7e9315b5a0304aebc1f1b7aa50
angelovelos-tu <angelo.velos@student.torrens.edu.au>
2026-07-15 16:08:17 +0930

    creating html for contact page, also added a feature to main task bar to be sticky, users can select options and will smoothly scroll to given section

--- Prototype/script.js
+++ Prototype/script.js
@@ -1,5 +1,17 @@
-function greet (name) {
-    alert('Hello, ' + name + '!');
-}
+const searchForm = document.getElementById('search-form');
+const searchMessage = document.getElementById('search-message');
 
-greet('Alice');
+const destinationInput = document.getElementById('destination');
+const guestsSelect = document.getElementById('guests');
+
+searchForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const destination = destinationInput.value.trim();
+    const guests = guestsSelect.value;
+
+    const guestLabel = guests === '1' ? 'guest' : 'guests';
+
+    searchMessage.textContent = 
+    `Searching for ${guests} ${guestLabel} in ${destination}...`;
+});
